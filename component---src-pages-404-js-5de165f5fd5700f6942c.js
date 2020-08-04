edfd6a141b086eaf917e7627322aec7de5d7dab1
Pete Chappell <pchappell@intellum.com>
2020-08-04 14:31:26 +0100

    Updates

---
+++ component---src-pages-404-js-5de165f5fd5700f6942c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(t,n,b){"use strict";b.r(n);b("q1tI");var e=b("Wbzz"),l=b("Bl7J"),o=b("vrFN"),u=b("qKvR");n.default=function(){return Object(u.b)(l.a,null,Object(u.b)(o.a,{title:"404: Not found"}),Object(u.b)("h1",null,"NOT FOUND"),Object(u.b)("p",null,"Yup. So it is."),Object(u.b)("p",null,"Try starting from ",Object(u.b)(e.Link,{to:"/"},"the home page"),"."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-5de165f5fd5700f6942c.js.map
--- gatsby-browser.js
+++
@@ -1,5 +0,0 @@
-// all pages should scroll to the top
-exports.shouldUpdateScroll = () => {
-  document.body.scrollTo(0, 0);
-  return false;
-};
